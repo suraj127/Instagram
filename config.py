12ce80dcc364f7ca349562d5554b36c34e151053
suraj127 <40730972+suraj127@users.noreply.github.com>
2025-08-20 11:30:55 +0530

    mm

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Instagram Credentials
 USERNAME = "inst.ascroll220"
-PASSWORD = "Suraj1234@"
+PASSWORD = "Gupta123@"
 
 # Search Settings
 SEARCH_KEYWORDS = ["goldjewellery", "travel", "fashion"]  # A list of hashtags or accounts to process
